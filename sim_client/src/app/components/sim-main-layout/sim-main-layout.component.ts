3923a621d15d28ffc0cabdb3b27b451977dc55b6
nguyenthang98 <nguyenducthang.uet@gmail.com>
2018-11-06 22:43:03 +0700

    update sample code

--- sim_client/src/app/components/sim-main-layout/sim-main-layout.component.ts
+++ sim_client/src/app/components/sim-main-layout/sim-main-layout.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { AppConfig } from 'src/app/models/app-config.model';
+import { CanvasUtilsService } from 'src/app/services/canvas-utils.service';
 
 @Component({
   selector: 'app-sim-main-layout',
   templateUrl: './sim-main-layout.component.html',
   styleUrls: ['./sim-main-layout.component.css']
 })
-export class SimMainLayoutComponent implements OnInit {
-  appConfig: AppConfig = new AppConfig(); 
+export class SimMainLayoutComponent {
+  appConfig: AppConfig; 
+  private containerId: string = "sim-work-area" + Date.now();
 
-  constructor() { }
+  constructor(private canvasUtils: CanvasUtilsService) { 
+    this.appConfig = new AppConfig(canvasUtils);
+  }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.appConfig.initStage(this.getContainerId())
+  }
+
+  getContainerId(): string {
+    return this.containerId;
   }
 
 }
--- sim_client/src/app/models/app-config.model.ts
+++ sim_client/src/app/models/app-config.model.ts
@@ -1,5 +1,7 @@
 import { ColorAdjustments } from './color-adjustments.model';
 import { LayerList } from './layer-list.model';
+import { Stage } from 'konva';
+import { CanvasUtilsService } from '../services/canvas-utils.service';
 
 export class AppConfig {
   mainConfig: {
@@ -10,8 +12,9 @@ export class AppConfig {
   adjustments: ColorAdjustments;
   layers: LayerList;
   currentFocusedObject: any;
+  stage: Stage;
 
-  constructor() {
+  constructor(private canvasUtils: CanvasUtilsService) {
     this.mainConfig = {
       width: 1080,
       height: 1080,
@@ -20,5 +23,16 @@ export class AppConfig {
     this.adjustments = new ColorAdjustments();
     this.layers = new LayerList(this.mainConfig);
     this.currentFocusedObject = null;
+    this.stage = null;
+  }
+
+  initStage(containerId) {
+    this.stage = this.canvasUtils.createStage(containerId, {width: this.mainConfig.width, height: this.mainConfig.height});
+
+    // add all existed layer
+    this.stage.add(this.layers.backgroundLayer);
+    this.layers.layerList.forEach(l => {
+      this.stage.add(l);
+    });
   }
 }
