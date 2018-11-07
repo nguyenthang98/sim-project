4c8d7473771382e7b48f7b9a075e77e2c87f35fc
nguyenthang98 <nguyenducthang.uet@gmail.com>
2018-11-07 18:22:22 +0700

    update heroku config

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const server = require('http').Server(app);
-const authApiRouter = require('./sim_server/routes/authentication.router');
-const imageApiRouter = require('./sim_server/routes/image.router');
 
-const port = 3000;
+// const authApiRouter = require('./sim_server/routes/authentication.router');
+// const imageApiRouter = require('./sim_server/routes/image.router');
 
 // Cross Origin Error
 app.use(cors());
@@ -20,13 +19,8 @@ app.use(
     express.static(path.join(__dirname, 'sim_client/dist/sim_client'))
 );
 
-app.use('/auth', authApiRouter);
-
-app.use(require('./sim_server/controllers/authentication').authenticate());
-app.use('/image', imageApiRouter);
-
-server.listen(port, () => {
-    console.log('Listening on port ' + port);
-});
+// app.use('/auth', authApiRouter);
+// app.use(require('./sim_server/controllers/authentication').authenticate());
+// app.use('/image', imageApiRouter);
 
 module.exports = app;
