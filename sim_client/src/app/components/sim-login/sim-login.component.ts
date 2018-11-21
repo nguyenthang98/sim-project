fa04711c0b935239b7b40ad93e8e9d3f7fbbde17
phucnguyenbn9x <phucnguyenbn.9x@gmail.com>
2018-11-21 11:22:25 +0700

    update

--- sim_client/src/app/components/sim-login/sim-login.component.ts
+++ sim_client/src/app/components/sim-login/sim-login.component.ts
@@ -9,111 +9,111 @@ import { MatSnackBar } from "@angular/material";
 const jwtHelper = new JwtHelperService();
 
 @Component({
-	selector: "app-sim-login",
-	templateUrl: "./sim-login.component.html",
-	styleUrls: ["./sim-login.component.css"]
+    selector: "app-sim-login",
+    templateUrl: "./sim-login.component.html",
+    styleUrls: ["./sim-login.component.css"]
 })
 export class SimLoginComponent implements OnInit {
-	currentSession: number;
-	rememberAuth: boolean;
-	checkValidPass: boolean;
+    currentSession: number;
+    rememberAuth: boolean;
+    checkValidPass: boolean;
 
-	loginForm: FormGroup;
-	registerForm: FormGroup;
+    loginForm: FormGroup;
+    registerForm: FormGroup;
 
-	constructor(
-		private simApiService: SimApiService,
-		private router: Router,
-		private snackBar: MatSnackBar
-	) {}
+    constructor(
+        private simApiService: SimApiService,
+        private router: Router,
+        private snackBar: MatSnackBar
+    ) { }
 
-	ngOnInit() {
-		this.initAuthInfo();
-		this.rememberAuth = false;
+    ngOnInit() {
+        this.initAuthInfo();
+        this.rememberAuth = false;
 
-		const token = localStorage.getItem("token") || "";
-		if (token && !jwtHelper.isTokenExpired(token)) {
-			this.router.navigate(["/"]);
-		}
-	}
+        const token = localStorage.getItem("token") || "";
+        if (token && !jwtHelper.isTokenExpired(token)) {
+            this.router.navigate(["/"]);
+        }
+    }
 
-	initAuthInfo(): void {
-		this.checkValidPass = true;
-		this.loginForm = new FormGroup({
-			username: new FormControl("", [Validators.required]),
-			password: new FormControl("", [])
-		});
+    initAuthInfo(): void {
+        this.checkValidPass = true;
+        this.loginForm = new FormGroup({
+            username: new FormControl("", [Validators.required]),
+            password: new FormControl("", [])
+        });
 
-		this.registerForm = new FormGroup({
-			username: new FormControl("", [Validators.required]),
-			password: new FormControl("", [Validators.required]),
-			email: new FormControl("", [Validators.required, Validators.email]),
-			confirmPassword: new FormControl("", [
-				Validators.required,
-				passValidator("password")
-			])
-		});
-	}
+        this.registerForm = new FormGroup({
+            username: new FormControl("", [Validators.required]),
+            password: new FormControl("", [Validators.required]),
+            email: new FormControl("", [Validators.required, Validators.email]),
+            confirmPassword: new FormControl("", [
+                Validators.required,
+                passValidator("password")
+            ])
+        });
+    }
 
-	changeCurrentSession(sessionCode: number): void {
-		this.initAuthInfo();
-		this.currentSession = sessionCode;
-	}
+    changeCurrentSession(sessionCode: number): void {
+        this.initAuthInfo();
+        this.currentSession = sessionCode;
+    }
 
-	onLoginButtonClicked(): void {
-		this.simApiService.simLogin(this.loginForm.value).subscribe(res => {
-			if (res.code !== 200) {
-				this.openSnackBar("Username or Password is incorrect", "Close");
-			} else {
-				if (this.rememberAuth) {
-					this.setUserInfo(localStorage, res.content);
-				} else {
-					localStorage.clear();
-					this.setUserInfo(sessionStorage, res.content);
-				}
-				this.router.navigate(["/"]);
-			}
-		});
-	}
+    onLoginButtonClicked(): void {
+        this.simApiService.simLogin(this.loginForm.value).subscribe(res => {
+            if (res.code !== 200) {
+                this.openSnackBar("Username or Password is incorrect", "Close");
+            } else {
+                if (this.rememberAuth) {
+                    this.setUserInfo(localStorage, res.content);
+                } else {
+                    localStorage.clear();
+                    this.setUserInfo(sessionStorage, res.content);
+                }
+                this.router.navigate(["/"]);
+            }
+        });
+    }
 
-	onRegisterButtonClicked(): void {
-		if (this.checkValidPass) {
-			this.simApiService.simRegister(this.registerForm.value).subscribe(res => {
-				if (res.code !== 200) {
-					if (res.content === "USER_EXISTED") {
-						this.openSnackBar("User existed", "Close");
-					} else if (res.content === "EMAIL_EXISTED") {
-						this.openSnackBar("Email existed", "Close");
-					}
-				} else {
-					this.setUserInfo(sessionStorage, res.content);
-					this.router.navigate(["/"]);
-				}
-			});
-		}
-	}
+    onRegisterButtonClicked(): void {
+        if (this.checkValidPass) {
+            this.simApiService.simRegister(this.registerForm.value).subscribe(res => {
+                if (res.code !== 200) {
+                    if (res.content === "USER_EXISTED") {
+                        this.openSnackBar("User existed", "Close");
+                    } else if (res.content === "EMAIL_EXISTED") {
+                        this.openSnackBar("Email existed", "Close");
+                    }
+                } else {
+                    this.setUserInfo(sessionStorage, res.content);
+                    this.router.navigate(["/"]);
+                }
+            });
+        }
+    }
 
-	private setUserInfo(storage: any, data: any) {
-		storage.setItem("token", data.token);
-		storage.setItem("idUser", data.idUser);
-		storage.setItem("username", data.username);
-		storage.setItem("avatar", data.avatar);
-	}
+    private setUserInfo(storage: any, data: any) {
+        storage.setItem("token", data.token);
+        storage.setItem("idUser", data.idUser);
+        storage.setItem("username", data.username);
+        storage.setItem("avatar", data.avatar);
+    }
 
-	getErrorMessage(form: string, field: string) {
-		return this[form].get(field).hasError("required")
-			? "You must enter a value"
-			: this[form].get(field).hasError("email")
-			? "Not a valid email"
-			: this[form].get(field).hasError("passValidator")
-			? "Password does not match"
-			: "";
-	}
+    getErrorMessage(form: string, field: string) {
+        return this[form].get(field).hasError("required")
+            ? "You must enter a value"
+            : this[form].get(field).hasError("email")
+                ? "Not a valid email"
+                : this[form].get(field).hasError("passValidator")
+                    ? "Password does not match"
+                    : "";
+    }
 
-	openSnackBar(message: string, action: string) {
-		this.snackBar.open(message, action, {
-			duration: 5000,
-			horizontalPosition: "end"
-		});
-	}
+    openSnackBar(message: string, action: string) {
+        this.snackBar.open(message, action, {
+            duration: 5000,
+            horizontalPosition: "end"
+        });
+    }
 }
--- sim_client/src/app/components/sim-user-collection/sim-user-collection.component.ts
+++ sim_client/src/app/components/sim-user-collection/sim-user-collection.component.ts
@@ -2,33 +2,33 @@ import { Component, OnInit } from "@angular/core";
 import { SimApiService } from "../../services/sim-api.service";
 
 @Component({
-	selector: "app-sim-user-collection",
-	templateUrl: "./sim-user-collection.component.html",
-	styleUrls: ["./sim-user-collection.component.css"]
+    selector: "app-sim-user-collection",
+    templateUrl: "./sim-user-collection.component.html",
+    styleUrls: ["./sim-user-collection.component.css"]
 })
 export class SimUserCollectionComponent implements OnInit {
-	constructor(private simApiService: SimApiService) {}
+    constructor(private simApiService: SimApiService) { }
 
-	avatarUrl: string;
+    avatarUrl: string;
 
-	ngOnInit() {
-		this.avatarUrl = `http://localhost:3000/${localStorage.avatar ||
-				sessionStorage.avatar}`;
-	}
+    ngOnInit() {
+        this.avatarUrl = `http://localhost:3000/${localStorage.avatar ||
+            sessionStorage.avatar}`;
+    }
 
-	changeAvatar() {
-		const inputFile = document.createElement("input");
-		inputFile.type = "file";
+    changeAvatar() {
+        const inputFile = document.createElement("input");
+        inputFile.type = "file";
 
-		inputFile.addEventListener("change", () => {
-			const file = <File>inputFile.files[0];
-			const payload = new FormData();
-			payload.append("file", file, file.name);
-			this.simApiService.changeAvatar(payload).subscribe(res => {
-				console.log(res);
-			});
-		});
+        inputFile.addEventListener("change", () => {
+            const file = <File>inputFile.files[0];
+            const payload = new FormData();
+            payload.append("file", file, file.name);
+            this.simApiService.changeAvatar(payload).subscribe(res => {
+                console.log(res);
+            });
+        });
 
-		inputFile.click();
-	}
+        inputFile.click();
+    }
 }
