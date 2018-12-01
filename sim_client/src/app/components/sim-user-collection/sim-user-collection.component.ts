2d541fa723eea3519fae8a8a80e0fb41a1220770
phucnguyenbn9x <phucnguyenbn.9x@gmail.com>
2018-12-01 22:05:26 +0700

    update

--- sim_client/src/app/components/sim-user-collection/sim-user-collection.component.ts
+++ sim_client/src/app/components/sim-user-collection/sim-user-collection.component.ts
@@ -3,45 +3,45 @@ import { SimApiService } from "../../services/sim-api.service";
 import { Router } from '@angular/router'
 
 @Component({
-    selector: "app-sim-user-collection",
-    templateUrl: "./sim-user-collection.component.html",
-    styleUrls: ["./sim-user-collection.component.css"]
+  selector: "app-sim-user-collection",
+  templateUrl: "./sim-user-collection.component.html",
+  styleUrls: ["./sim-user-collection.component.css"]
 })
 export class SimUserCollectionComponent implements OnInit {
-    constructor(private simApiService: SimApiService, private router: Router) { }
+  constructor(private simApiService: SimApiService, private router: Router) { }
 
-    avatarUrl: string;
-    userDesigns: Array<any>;
-    localUrl: string;
+  avatarUrl: string;
+  userDesigns: Array<any>;
+  localUrl: string;
 
-    ngOnInit() {
-        this.localUrl = 'http://localhost:3000';
-        this.localUrl = '';
-        this.avatarUrl = `${this.localUrl}/${localStorage.avatar || sessionStorage.avatar}`;
+  ngOnInit() {
+    this.localUrl = 'http://localhost:3000';
+    // this.localUrl = '';
+    this.avatarUrl = `${this.localUrl}/${localStorage.avatar || sessionStorage.avatar}`;
 
-        this.simApiService.listImages().subscribe(res => {
-            this.userDesigns = res.content;
-        });
-    }
+    this.simApiService.listImages().subscribe(res => {
+      this.userDesigns = res.content;
+    });
+  }
 
-    changeAvatar() {
-        const inputFile = document.createElement("input");
-        inputFile.type = "file";
+  changeAvatar() {
+    const inputFile = document.createElement("input");
+    inputFile.type = "file";
 
-        inputFile.addEventListener("change", () => {
-            const file = <File>inputFile.files[0];
-            const payload = new FormData();
-            payload.append("file", file, file.name);
-            this.simApiService.changeAvatar(payload).subscribe(res => {
-                if (localStorage.getItem('avatar')) {
-                    localStorage.setItem('avatar', res.content);
-                } else {
-                    sessionStorage.setItem('avatar', res.content);
-                }
-                location.reload();
-            });
-        });
+    inputFile.addEventListener("change", () => {
+      const file = <File>inputFile.files[0];
+      const payload = new FormData();
+      payload.append("file", file, file.name);
+      this.simApiService.changeAvatar(payload).subscribe(res => {
+        if (localStorage.getItem('avatar')) {
+          localStorage.setItem('avatar', res.content);
+        } else {
+          sessionStorage.setItem('avatar', res.content);
+        }
+        location.reload();
+      });
+    });
 
-        inputFile.click();
-    }
+    inputFile.click();
+  }
 }
--- sim_client/src/app/sim-auth.guard.ts
+++ sim_client/src/app/sim-auth.guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import {
-    CanActivate,
-    ActivatedRouteSnapshot,
-    RouterStateSnapshot,
-    Router
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
 } from "@angular/router";
 import { Observable } from "rxjs";
 import { SimApiService } from "./services/sim-api.service";
@@ -12,23 +12,23 @@ import { JwtHelperService } from "@auth0/angular-jwt";
 const jwtHelper = new JwtHelperService();
 
 @Injectable({
-    providedIn: "root"
+  providedIn: "root"
 })
 export class SimAuthGuard implements CanActivate {
-    constructor(private simApiService: SimApiService, private router: Router) { }
+  constructor(private simApiService: SimApiService, private router: Router) { }
 
-    canActivate(
-        next: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
-    ): Observable<boolean> | Promise<boolean> | boolean {
-        // return true;
-        console.log({ next, state });
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    // return true;
+    console.log({ next, state });
 
-        const token = localStorage.getItem("token") || sessionStorage.getItem("token");
-        if (token && !jwtHelper.isTokenExpired(token)) {
-            return true;
-        }
-        this.router.navigate(["/login"]);
-        return false;
+    const token = localStorage.getItem("token") || sessionStorage.getItem("token");
+    if (token && !jwtHelper.isTokenExpired(token)) {
+      return true;
     }
+    this.router.navigate(["/login"]);
+    return false;
+  }
 }
