f9bbfc423e4f8f2174ce09bbe3e340eb0af04acb
TranQuangToan <toantran.249x@gmail.com>
2018-12-12 09:49:04 +0700

    update header homepage

--- sim_client/src/app/components/sim-homepage/sim-homepage.component.ts
+++ sim_client/src/app/components/sim-homepage/sim-homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: 'app-sim-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sim-homepage.component.css']
 })
 export class SimHome implements OnInit {
+    isLogin: boolean;
+    constructor(private httpClient: HttpClient) {
+        let token = localStorage.getItem('token') || sessionStorage.getItem('token');
+        if (token) {
+          this.isLogin = true;
+        } else {
+          this.isLogin = false;
+        }
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
